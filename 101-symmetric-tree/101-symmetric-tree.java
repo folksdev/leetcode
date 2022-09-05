3519fabc951ccabd4bc3342cd8ed9010908f5179
folksdev <73724185+folksdev@users.noreply.github.com>
2022-09-05 22:00:05 +0200

    Time: 1 ms (63.91%), Space: 43 MB (10.76%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -15,22 +15,33 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        return traverse(root.left, true).equals(traverse(root.right, false));
+        return traverse(root.left, root.right);
     }
 
-    public String traverse(TreeNode root, boolean left) {
+    public boolean traverse(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val) && traverse(p.left, q.right) && traverse(p.right, q.left);
+    }
+
+    //Alternative
+    public boolean isSymmetric2(TreeNode root) {
+        return traverse2(root.left, true).equals(traverse2(root.right, false));
+    }
+
+    public String traverse2(TreeNode root, boolean left) {
         if (root == null) {
             return " ";
         }
         String result = "";
         if (left) {
-            result += traverse(root.left,true);
-            result += traverse(root.right, false);
+            result += traverse2(root.left, true);
+            result += traverse2(root.right, false);
         } else {
-            result += traverse(root.right, false);
-            result += traverse(root.left,true);
+            result += traverse2(root.right, false);
+            result += traverse2(root.left, true);
         }
-        result  +=  root.val;
+        result += root.val;
         return result;
     }
 }

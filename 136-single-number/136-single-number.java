6e03ae3474e17003cb137fb0fa5a135df4b66397
folksdev <73724185+folksdev@users.noreply.github.com>
2022-09-13 21:09:24 +0200

    Time: 1317 ms (5.00%), Space: 54.6 MB (7.67%) - LeetHub

---
+++ 136-single-number/136-single-number.java
@@ -0,0 +1,16 @@
+import java.util.Optional;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        List<Integer> s = new ArrayList<>();
+        s.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            if (s.contains(nums[i])){
+                s.remove((Integer) nums[i]);
+            } else {
+                s.add(nums[i]);
+            }
+        }
+        return Optional.ofNullable(s.get(0)).orElse(0);
+    }
+}

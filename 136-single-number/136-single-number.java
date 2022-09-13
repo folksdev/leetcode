f055e20b2a279cacde883a7767a65ff5ec396409
folksdev <73724185+folksdev@users.noreply.github.com>
2022-09-13 21:51:01 +0200

    Time: 23 ms (17.03%), Space: 42.7 MB (88.13%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,7 +1,8 @@
 import java.util.Optional;
 
 class Solution {
-    public int singleNumber(int[] nums) {
+    
+   public int singleNumber2(int[] nums) {
         List<Integer> s = new ArrayList<>();
         s.add(nums[0]);
         for (int i = 1; i < nums.length; i++) {
@@ -9,8 +10,20 @@ class Solution {
                 s.remove((Integer) nums[i]);
             } else {
                 s.add(nums[i]);
+
             }
         }
         return s.get(0);
+    }
+
+    //Alternative
+    public int singleNumber(int[] nums) {
+        Map<Integer, Long> map = Arrays.stream(nums).boxed().collect(Collectors.groupingBy(e -> e, Collectors.counting()));
+        for (Map.Entry<Integer, Long> e: map.entrySet()) {
+            if (e.getValue() == 1) {
+                return e.getKey();
+            }
+        }
+        return -1;
     }
 }

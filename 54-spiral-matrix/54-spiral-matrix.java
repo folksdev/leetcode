c146ce34a09febf4c41a8c60364b203f1c579947
folksdev <73724185+folksdev@users.noreply.github.com>
2022-10-04 02:13:09 +0200

    Time: 0 ms (100.00%), Space: 42.6 MB (6.94%) - LeetHub

---
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] arr) {
+        int T = 0, B =  arr.length-1, L = 0, R = arr[0].length -1, dir = 0;
+        List<Integer> result = new ArrayList<>();
+        while (T <= B && L <= R) {
+            if (dir == 0) { //Traverse left to right
+                for (int i = L; i <= R; i++) {
+                    result.add(arr[T][i]);
+                }
+                T++;
+            } else if (dir == 1) { //Traverse right to bottom
+                for (int i = T; i <= B; i++) {
+                    result.add(arr[i][R]);
+                }
+                R--;
+            } else if (dir == 2) { // right to left
+                for (int i = R; i >= L; i--){
+                    result.add(arr[B][i]);
+                }
+                B--;
+            } else if (dir == 3) { //bottom to top
+                for (int i = B; i >= T; i--) {
+                    result.add(arr[i][L]);
+                }
+                L++;
+            }
+            dir = (dir + 1) % 4; //1 = next direction, 4 = direction count
+        }
+        return result;
+    }
+}

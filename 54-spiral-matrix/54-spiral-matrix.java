519c395c7cb10857d10639c09090718dc519ecce
folksdev <73724185+folksdev@users.noreply.github.com>
2022-10-04 02:13:59 +0200

    Time: 0 ms (100.00%), Space: 42.2 MB (34.83%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,5 +1,8 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] arr) {
+        //Time Complexity: O(M*N). To traverse the matrix O(M*M) time is required.
+        //Auxiliary Space: O(1). No extra space is required.
+        //T = Top corner, B = bottom corner, L = Left corner, R = right corner, dir = direction
         int T = 0, B =  arr.length-1, L = 0, R = arr[0].length -1, dir = 0;
         List<Integer> result = new ArrayList<>();
         while (T <= B && L <= R) {
